c6daa6c15d085d895392eb5087f5919fe9991e45
stevepopovich <stevepopovich8@gmail.com>
2020-01-24 11:14:58 -0500

    first commit

---
+++ getoff/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  getoff
+//
+//  Created by Steven Popovich on 1/24/20.
+//  Copyright © 2020 Steven Popovich. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
