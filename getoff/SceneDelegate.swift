b0a0d16dcff2fd935c3741e48d6c4aa60bb1baad
steven.popovich <steven.popovich@gc.com>
2020-01-24 15:43:55 -0500

    clean up

--- getoff/SceneDelegate.swift
+++ getoff/SceneDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import SwiftUI
 import CoreLocation
+import UserNotifications
 
 struct DerivedSubwayData : Codable {
     let stationName: String
@@ -45,27 +46,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate, CLLocationManagerDelega
     var subwayLocations: [DerivedSubwayData] = []
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-
-        // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView(text: "Hello World!")
 
-        // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
             window.rootViewController = UIHostingController(rootView: contentView)
             self.window = window
             window.makeKeyAndVisible()
         }
-    }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
@@ -81,53 +69,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate, CLLocationManagerDelega
             let subwayData = try! JSONDecoder().decode([DerivedSubwayData].self, from: stringOne)
             self.subwayLocations = subwayData
         } else {
-            var urlRequest = URLRequest(url: URL(string: "https://data.cityofnewyork.us/api/views/kk4q-3rt2/rows.json?accessType=DOWNLOAD")!)
-
-            urlRequest.httpMethod = "GET"
-            urlRequest.setValue("application/json", forHTTPHeaderField: "Accept")
-
-            let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
-
-                guard let data = data else { return }
-                let subwayData = try? JSONDecoder().decode(SubwayData.self, from: data)
-
-                guard let strongSubwayData = subwayData else { return }
-
-                print(strongSubwayData.data[0][10])// Name is 10, location is 11
-                print(strongSubwayData.data[0][11])// Name is 10, location is 11
-                print(strongSubwayData.data)
-                print(strongSubwayData.data[0].count)
-
-                let subwayDerivedData = self.transformSubwayDataToUsefulStuff(strongSubwayData)
-
-                let defaults = UserDefaults.standard
-
-                do {
-                    try defaults.set(JSONEncoder().encode(subwayDerivedData), forKey: DerivedSubwayDataKeys.derivedDataKey)
-
-                    self.subwayLocations = subwayDerivedData
-                } catch {
-                    fatalError()
-                }
-            }
-            task.resume()
+            getSubwayDataFromTheInternet()
         }
+    }
+
+    private func registerForPushNotifications() {
+        UNUserNotificationCenter.current() // 1
+                .requestAuthorization(options: [.alert, .sound, .badge]) { // 2
+                    granted, error in
+                    print("Permission granted: \(granted)") // 3
+                }
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
     }
 
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
@@ -141,7 +97,41 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate, CLLocationManagerDelega
 
             let closestSubway = self.getClosestSubwayStation(location: location)
             window!.rootViewController = UIHostingController(rootView: ContentView(text: closestSubway.stationName))
+            print(closestSubway)
         }
+    }
+
+    private func getSubwayDataFromTheInternet() {
+        var urlRequest = URLRequest(url: URL(string: "https://data.cityofnewyork.us/api/views/kk4q-3rt2/rows.json?accessType=DOWNLOAD")!)
+
+        urlRequest.httpMethod = "GET"
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Accept")
+
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+
+            guard let data = data else { return }
+            let subwayData = try? JSONDecoder().decode(SubwayData.self, from: data)
+
+            guard let strongSubwayData = subwayData else { return }
+
+            print(strongSubwayData.data[0][10])// Name is 10, location is 11
+            print(strongSubwayData.data[0][11])// Name is 10, location is 11
+            print(strongSubwayData.data)
+            print(strongSubwayData.data[0].count)
+
+            let subwayDerivedData = self.transformSubwayDataToUsefulStuff(strongSubwayData)
+
+            let defaults = UserDefaults.standard
+
+            do {
+                try defaults.set(JSONEncoder().encode(subwayDerivedData), forKey: DerivedSubwayDataKeys.derivedDataKey)
+
+                self.subwayLocations = subwayDerivedData
+            } catch {
+                fatalError()
+            }
+        }
+        task.resume()
     }
 
     private func getClosestSubwayStation(location: CLLocation) -> DerivedSubwayData {
@@ -167,7 +157,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate, CLLocationManagerDelega
             if case .string(let stationNameValue) = stationName {
                 if case .string(let pointValue) = pointString {
                     let splitThing = pointValue.split(separator: " ")
-                    print(splitThing)
                     let lat = CGFloat(Double(splitThing[1].suffix(splitThing[1].count - 2))!)
                     let lng = CGFloat(Double(splitThing[2].prefix(while: { $0 != ")" }))!)
 
